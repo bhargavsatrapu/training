3f4f6b4e1898fced059e64c480f698c9b6c3ba4e
bhargavsatrapu <bhargav.satrapu@feuji.com>
2022-12-05 17:04:18 +0530

    forkify task

---
+++ forkifyCode.js
@@ -0,0 +1,76 @@
+
+
+
+var pageNo=1;
+
+ var recipeList=document.querySelector(".recipe-list");
+ var previousButton=document.querySelector(".previous-button");
+ var nextButton=document.querySelector(".next-button");
+ var previousPage=document.querySelector(".previous-page");
+ var nextPage=document.querySelector(".next-page");
+ 
+ 
+  previousButton.addEventListener("click",pagePrevious);
+  nextButton.addEventListener("click",pageNext);
+ 
+ 
+ 
+ 
+ 
+ 
+ previousButton.style.display="none";
+ dispayRecipeMenu();
+ 
+ function dispayRecipeMenu(){
+ 
+   for(var i=(pageNo*10)-10;i<pageNo*10;i++){	   
+	   recipeList.innerHTML+=`<li>
+	                                               <div class="recipe-menu">
+															<div class="recipe-image">
+																<img src="${jData.recipes[i].image_url}" alt="hai" class="menu-image">
+															</div>
+															<div class="recipe-content">
+																<h4>${jData.recipes[i].title}</h4>
+																<p>${jData.recipes[i].publisher}g</P>
+															</div>
+													</div>      
+												</li>	`
+   }
+   
+ }
+ 
+ function pagePrevious(){
+	  pageNo--;
+	  previousPage.innerHTML=`page ${pageNo-1}`;
+	  nextPage.innerHTML=`page ${pageNo+1}`;
+	 if((pageNo*10)==10){
+		 previousButton.style.display="none";
+	     nextButton.style.display="block";
+	    recipeList.innerHTML="";
+	    dispayRecipeMenu();
+		
+	 }else{
+	  nextButton.style.display="block";
+	  recipeList.innerHTML="";
+	  dispayRecipeMenu(); 
+	 }
+ }
+ 
+ 
+  function pageNext(){
+	   pageNo++;
+	   previousPage.innerHTML=`page ${pageNo-1}`;
+	   nextPage.innerHTML=`page ${pageNo+1}`;
+	   if((pageNo*10)>=jData.count){
+		 nextButton.style.display="none";
+	     previousButton.style.display="block";
+	     recipeList.innerHTML="";
+	     dispayRecipeMenu();
+	   }else{
+	    previousButton.style.display="block";
+	    recipeList.innerHTML="";
+	    dispayRecipeMenu();
+	   }
+ }
+ 
+ 
